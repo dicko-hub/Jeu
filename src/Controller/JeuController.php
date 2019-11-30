421e7a7e903e4ae3f6b70901a5c9320b9950fbf7
dicko-hub <dicko-hub@users.noreply.github.com>
2019-11-30 09:03:04 +0100

    creation de mon fichier controleur ainsi que de sa page d'affichage

---
+++ src/Controller/JeuController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class JeuController extends Controller
+{
+    /**
+     * @Route("/jeu", name="jeu")
+     */
+    public function index()
+    {
+        return $this->render('jeu/index.html.twig', [
+            'controller_name' => 'JeuController',
+        ]);
+    }
+}
