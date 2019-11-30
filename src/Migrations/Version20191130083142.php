8c3896a6932da18bcda573419ecd8a4f0eff58e3
dicko-hub <dicko-hub@users.noreply.github.com>
2019-11-30 09:40:18 +0100

    creation de la classe Salle, de son fichier de controle et de migration

---
+++ src/Migrations/Version20191130083142.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191130083142 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE salle (id INT AUTO_INCREMENT NOT NULL, description VARCHAR(255) NOT NULL, passages LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE personnage ADD salle INT ');
+        $this->addSql('ALTER TABLE personnage ADD CONSTRAINT FK_23A0E6612469DE3 FOREIGN KEY (salle) REFERENCES salle (id)');
+        $this->addSql('CREATE INDEX IDX_23A0E6612469DE3 ON personnage (salle)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE salle');
+    }
+}
