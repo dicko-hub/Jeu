d7c5b7004cd6a2e00fdc58ea963cfe2c115c3428
dicko-hub <dicko-hub@users.noreply.github.com>
2019-11-30 09:12:36 +0100

    creation de ma classe Personnage et de son fichier de controle afin de le migrer vers la base

---
+++ src/Entity/Personnage.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PersonnageRepository")
+ */
+class Personnage
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $guid;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $degats;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $vie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $totalVie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGuid(): ?int
+    {
+        return $this->guid;
+    }
+
+    public function setGuid(int $guid): self
+    {
+        $this->guid = $guid;
+
+        return $this;
+    }
+
+    public function getDegats(): ?int
+    {
+        return $this->degats;
+    }
+
+    public function setDegats(int $degats): self
+    {
+        $this->degats = $degats;
+
+        return $this;
+    }
+
+    public function getVie(): ?int
+    {
+        return $this->vie;
+    }
+
+    public function setVie(?int $vie): self
+    {
+        $this->vie = $vie;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTotalVie(): ?int
+    {
+        return $this->totalVie;
+    }
+
+    public function setTotalVie(int $totalVie): self
+    {
+        $this->totalVie = $totalVie;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
